f018ed0150af619be9a094c16e8eb87df960951b
fayzzz <mustafo.fayz@yandex.ru>
2020-07-10 09:20:17 +0500

    Structured folders and made readonly object properties as enum

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+export function actionTypesFactory<T extends string[]>(
+  prefix: string,
+  ...actionType: T
+): {
+  readonly [K in T[number]]: {
+    default: K;
+    begin: string;
+    succeeded: string;
+    failed: string;
+    cancelled: string;
+  };
+};
+
+export function actionTypesFactory(
+  prefix: string,
+  ...actionType: string[]
+): {
+  readonly [k: string]: {
+    default: string;
+    begin: string;
+    succeeded: string;
+    failed: string;
+    cancelled: string;
+  };
+} {
+  const prefixString = prefix.length ? `${prefix}/` : "";
+  return actionType.reduce(
+    (acc, type) => ({
+      ...acc,
+      [type]: {
+        default: `${prefixString}${type}`,
+        begin: `${prefixString}${type}_BEGIN`,
+        succeeded: `${prefixString}${type}_SUCCEEDED`,
+        failed: `${prefixString}${type}_FAILED`,
+        cancelled: `${prefixString}${type}_CANCELLED`,
+      },
+    }),
+    {}
+  );
+}
